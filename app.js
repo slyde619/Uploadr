7033d8ca72bbfce05f48557985e93481cfe66082
slyde619 <slyde619@gmail.com>
2023-02-27 16:01:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const data = document.querySelector('.data')
+const state = document.querySelector('#state')
+const unit = document.querySelector("#unit");
+const file = document.querySelector("#file");
+const submit = document.querySelector("#submit");
